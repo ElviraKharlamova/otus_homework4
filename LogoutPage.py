9d037d4b6605ecfa0cbaba14c4864bab40543ec3
Elvira_Kharlamova <EHarlamova@mtt.ru>
2023-05-31 00:16:09 +0300

    refactor

--- LogoutPage.py
+++
@@ -1,7 +0,0 @@
-
-class LogoutPage:
-    def __init__(self, driver):
-        self.driver = driver
-
-    def logout(self, driver):
-        driver.get("http://127.0.0.1:8081/index.php?route=account/logout")
